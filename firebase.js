b508304e08254f60e7e9cdfe2c1b2c72a35a88da
emma123456789102 <emma.davidson0408@gmail.com>
2025-03-05 18:07:20 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDbyxievV9aKGHjLFyO0BKKv4V8rRd5P-k",
+  authDomain: "autismdiagnosistool.firebaseapp.com",
+  projectId: "autismdiagnosistool",
+  storageBucket: "autismdiagnosistool.firebasestorage.app",
+  messagingSenderId: "932313058973",
+  appId: "1:932313058973:web:fbb31ab2cf6ef0ae700e74",
+  measurementId: "G-VDYP4DYPXB"
 };
 
 const app = initializeApp(firebaseConfig);
